ecd6e33eb1100d846187b45c2fecf37cbc36f0b6
bakamedi <bakamedi12@gmail.com>
2015-07-15 23:59:01 -0500

    Cambio en Perfil
    cambios en el perfil y el javascript

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,3 +1,5 @@
+var userProfileList = [];
+
 function inicializar() {
     var btnGuardar = document.getElementById("btnGuardar");
     btnGuardar.addEventListener('click',guardaDatosLabel, false);
@@ -6,6 +8,7 @@ function inicializar() {
     var btnGuardar = document.getElementById("guardar");
     btnGuardar.setAttribute("style", "display: none;")
 }
+
 function habilitaBotonesPerfil(){
     var nombrePerfil = document.getElementById("nombrePerfil");
     nombrePerfil.style.display = 'none';
@@ -81,9 +84,12 @@ function guardaDatosLabel(){
         apellidoPerfil.innerHTML = inputApellido.value;
         
         inputPlacaPerfil.innerHTML = inputPlaca.value;
-        msmErrorApellido.innerHTML = "";        msmErrorNombre.innerHTML = "";
+        msmErrorApellido.innerHTML = "";
+        msmErrorNombre.innerHTML = "";
         var btnGuardar = document.getElementById("guardar");
-        btnGuardar.setAttribute("style", "display: none;");    }
+        btnGuardar.setAttribute("style", "display: none;");
+    }
+
 
 
 }
@@ -156,13 +162,41 @@ function ocultaCancelar(){
     inputPlaca.style.display = 'none';
     var btnGuardar = document.getElementById("guardar");
     btnGuardar.setAttribute("style", "display: none;");
-
     msmError.innerHTML = "";
-    msmErrorApellido.innerHTML = "";    msmErrorNombre.innerHTML = "";
+    msmErrorApellido.innerHTML = "";
+    msmErrorNombre.innerHTML = "";
     inputNombre.innerHTML = 'none';
     inputApellido.innerHTML = 'none';
     inputCarro.innerHTML = 'none';
     inputPlaca.innerHTML = 'none';
+}
+
+function obtenerDatosPerfil(){
+    var id,nombre,apellido,imagenSrc,seguidores,seguidos,tieneCarro,placa;
+    nombre = document.getElementById("labelNombre");
+    apellido = document.getElementById("apellidoPerfil");
+    seguidores = document.getElementById("labelSeguidores");
+    seguidos = document.getElementById("labelSiguiendo");
+    tieneCarro = document.getElementById("carroPerfil");
+    placa = document.getElementById("input-placa-Perfil");
+    imagenSrc = document.getElementById("imaUser");
+    
+    nombre.innerHTML = userProfileList[0];
+    apellido.innerHTML = userProfileList[1];
+    seguidores.innerHTML = userProfileList[2];
+    seguidos.innerHTML = userProfileList[3];
+    imagenSrc.setAttribute("src", userProfileList[5]);
+    if(userProfileList[4]=="si" || userProfileList[4]=="Si"){
+        placa.innerHTML = userProfileList[6];
+    }
+}
+
+function perfil() {
+    location.href = "perfil.html";
+}
+
+function seguidores() { 
+    location.href = "main_seguidores.html";
 }
 
 window.addEventListener('load', inicializar, false);
