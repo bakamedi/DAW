60d3c3611828dfbd8ed77ac413445bd5030e96a5
Gabriel Aumala <gabriel@criptext.com>
2015-08-26 00:19:52 -0500

    Sessions
    Sessions implementadas con el session client de Mozilla. '/inicio' esta protegido por login
    se revisa en el session que el usuario este logueado. asi mismo. la pagina de Log in redirige
    a inicio si el usuario ya esta logueado.

--- node/app.js
+++ node/app.js
@@ -2,7 +2,7 @@ var express 		= require('express');
 var bodyParser     	= require('body-parser');
 var morgan         	= require('morgan');
 var methodOverride 	= require('method-override');
-
+var sessions = require("client-sessions");
 var soap 			= require('soap');
 
 var app 			= express();
@@ -17,11 +17,32 @@ app.use(bodyParser.urlencoded({ extended: false }));    // parse application/x-w
 app.use(bodyParser.json());    							// parse application/json
 app.use(methodOverride());
 
+app.use(sessions({
+  cookieName: 'carPoolSession', // cookie name dictates the key name added to the request object
+  secret: 'tDSg8Iw8hcODgqK6olqL2vUiCqxoKPt1LgBWv8G6wSvTA0d7IV3ZWfbf5oENpcO', // should be a large unguessable string
+  duration: 30 * 60 * 1000, // how long the session will stay valid in ms
+  activeDuration: 1000 * 60 * 15 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+}));
+
+/**
+* Pagina de login
+**/
 app.get('/', function (req, res) {
-  res.render('login.jade')
+  if(req.carPoolSession.username != null)
+        res.redirect('/inicio');
+  else
+        res.render('login.jade')
 })
 
-
+/**
+* Pagina de perfil
+**/
+app.get('/inicio', function (req, res) {
+  if(req.carPoolSession.username == null)
+        res.redirect('/');
+  else
+        res.render('perfil.jade')
+})
 
 var url = 'http://ws.espol.edu.ec/saac/wsandroid.asmx?WSDL';
 app.post('/inicio', function (req, res){
@@ -30,11 +51,10 @@ app.post('/inicio', function (req, res){
 	  	client.autenticacion(args, function(err, result) { 
 	  		re = result.autenticacionResult;
 	  		if(re){
+                                req.carPoolSession.username = req.body.Email; //Coloco el username en el session
 	  			res.render('perfil.jade',req.body.Email);
 	  		}
 	  		else{
-	  			var f = misc.x();
-	  			console.log(f);
 	  			res.redirect('/');
 	  		}
 	  			
