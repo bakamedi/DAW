5f4c5e292f4691f77a8850754d802ceae7ddbbbc
bakamedi <bakamedi12@gmail.com>
2015-09-05 02:22:25 -0500

    cambios en perfil
    quite los fonts,y se rehizo el perfil y el nav de mis rutas es solo
    pantalla pero es un estilo para poder poner las rutas

--- node/public/javascripts/perfil_val.js
+++ node/public/javascripts/perfil_val.js
@@ -17,8 +17,33 @@ function archivo(evt) {
     }
 }
 
-function verBotonGuardarImagen(){
-	
+function habilitaImagenDiv(){
+    var formularioImagen = document.getElementById("formularioImagen");
+    formularioImagen.setAttribute("style","display:normal;");
 }
 
+function desabilitaImagenDiv(){
+    var formularioImagen = document.getElementById("formularioImagen");
+    formularioImagen.setAttribute("style","display:none;");
+}
+
+function initialize()
+{
+  var mapProp = {
+    center: new google.maps.LatLng(51.508742,-0.120850),
+    zoom:7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+function loadScript()
+{
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=&sensor=false&callback=initialize";
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
 document.getElementById('files').addEventListener('change', archivo, false);
