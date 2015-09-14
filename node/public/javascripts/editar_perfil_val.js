351f8e243ee59b372cafeb0ea4421fd03d43357c
bakamedi <bakamedi12@gmail.com>
2015-09-14 18:04:50 -0500

    Cambios Editar Perfil
    hice unas modificaciones que molestaban al momento de editar al usuario

--- node/public/javascripts/editar_perfil_val.js
+++ node/public/javascripts/editar_perfil_val.js
@@ -2,6 +2,11 @@ function inicializar() {
 
 }
 
+function ponerImagen(){
+    var onixfile = document.getElementById("onixfile").value;
+    var imaUser = document.getElementById("imaUser");
+    imaUser.setAttribute("src",onixfile);
+}
 
 function validarTengoCarro(){
     var divPlaca = document.getElementById("formInPlaca");
@@ -91,6 +96,75 @@ function validarPlacaDetener() {
     }
 }
 
+function validarPalabraNombre() {
+    var palabra      = document.getElementById("nombrePerfil").value;
+    var estadoNombre = document.getElementById("msmErrorNombre");
+
+    if(palabra.length != 0) {
+        for(i=0; i<palabra.length; i++) {
+            if(palabra[i]>="0" && palabra[i]<= palabra.length){
+                estadoNombre.innerHTML = "El Nombre debe de tener Numeros";
+                estadoNombre.style.color = "red";
+            }
+            if((palabra[i]>="A" && palabra[i]<= "Z") || (palabra[i]>="a" && palabra[i]<= "z")){
+                estadoNombre.innerHTML = "";
+            }
+        }
+    }
+    else { 
+        estadoNombre.innerHTML = "El Nombre no debe estar Vacio";
+        estadoNombre.style.color = "red";
+    }
+}
+
+function validarPalabraApellido() {
+    var palabra      = document.getElementById("apellidoPerfil").value;
+    var estadoNombre = document.getElementById("msmErrorApellido");
+
+    if(palabra.length != 0) {
+        for(i=0; i<palabra.length; i++) {
+            if(palabra[i]>="0" && palabra[i]<= palabra.length){
+                estadoNombre.innerHTML = "El Nombre debe de tener Numeros";
+                estadoNombre.style.color = "red";
+            }
+            if((palabra[i]>="A" && palabra[i]<= "Z") || (palabra[i]>="a" && palabra[i]<= "z")){
+                estadoNombre.innerHTML = "";
+            }
+        }
+    }
+    else { 
+        estadoNombre.innerHTML = "El Nombre no debe estar Vacio";
+        estadoNombre.style.color = "red";
+    }
+}
+
+function validarPlaca() {
+    var placa = document.getElementById("inPlaca").value;
+    var conNum = 0;
+    var conLetras = 0;
+
+    var estadoNombre = document.getElementById("msmError");
+
+    if(placa.length == 7) {
+        for(i=0; i<7; i++) {
+            if(placa[i]>="0" && placa[i]<= "9"){
+                conNum++;
+            }
+            if(placa[i]>="A" && placa[i]<= "Z"){
+                conLetras++;
+            }
+        }
+    }
+
+    if (conLetras + conNum == 7 && conLetras == 3 && conNum == 4) {
+        estadoNombre.innerHTML = "";
+        //estadoNombre.style.color = "red";
+    }
+    else { 
+        estadoNombre.innerHTML = "Placa incorrecta";
+        estadoNombre.style.color = "red";
+    }
+}
 
 function justNumbers(e){
     var keynum = window.event ? window.event.keyCode : e.which;
